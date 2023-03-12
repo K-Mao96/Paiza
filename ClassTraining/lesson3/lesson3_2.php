46d62b68592dd1307d8393304a1cf4f27fae17c7
K.M <indiangirl.0914@gmail.com>
2023-03-12 15:49:53 +0900

    PHPDocsと型宣言を追加

--- ClassTraining/lesson3/lesson3_2.php
+++ ClassTraining/lesson3/lesson3_2.php
@@ -1,85 +1,171 @@
 <?php
 
-    //勇者クラス
+    /**
+     * 勇者クラス
+     */
     class Brave {
 
-        //コンストラクタ
-        //ステータスの初期値をセットする
+        /**
+         * コンストラクタ
+         *
+         * 勇者のステータスに初期値をセットする
+         * @param integer $level            レベル
+         * @param integer $physicalStrength 体力
+         * @param integer $offensivePower   攻撃力
+         * @param integer $defense          防御力
+         * @param integer $agility          素早さ
+         * @param integer $cleverness       賢さ
+         * @param integer $luck             運
+         */
         public function __construct(
-            private int $level,            //レベル
-            private int $physicalStrength, //体力
-            private int $offensivePower,   //攻撃力
-            private int $defense,          //防御力
-            private int $agility,          //素早さ
-            private int $cleverness,       //賢さ
-            private int $luck              //運
+            private int $level,
+            private int $physicalStrength,
+            private int $offensivePower,
+            private int $defense,
+            private int $agility,
+            private int $cleverness,
+            private int $luck
         ) {}
 
-        //レベルをアップする
-        public function levelUp($value = 1) {
+        /**
+         * 勇者のレベルをアップする
+         *
+         * @param integer $value レベルの増分
+         * @return void
+         */
+        public function levelUp(int $value = 1): void {
             $this->level += $value;
         }
 
-        //体力をアップする
-        public function physicalStrengthUP($value) {
+        /**
+         * 勇者の体力をアップする
+         *
+         * @param integer $value 体力の増分
+         * @return void
+         */
+        public function physicalStrengthUP(int $value): void {
             $this->physicalStrength += $value;
         }
 
-        //攻撃力をアップする
-        public function offensivePowerUp($value) {
+        /**
+         * 勇者の攻撃力をアップする
+         *
+         * @param integer $value 攻撃力の増分
+         * @return void
+         */
+        public function offensivePowerUp(int $value): void {
             $this->offensivePower += $value;
         }
 
-        //防御力をアップする
-        public function defenseUp($value) {
+        /**
+         * 勇者の防御力をアップする
+         *
+         * @param integer $value 防御力の増分
+         * @return void
+         */
+        public function defenseUp(int $value): void {
             $this->defense += $value;
         }
 
-        //素早さをアップする
-        public function agilityUp($value) {
+        /**
+         * 勇者の素早さをアップする
+         *
+         * @param integer $value 素早さの増分
+         * @return void
+         */
+        public function agilityUp(int $value): void {
             $this->agility += $value;
         }
 
-        //賢さをアップする
-        public function clevernessUp($value) {
+        /**
+         * 勇者の賢さをアップする
+         *
+         * @param integer $value 賢さの増分
+         * @return void
+         */
+        public function clevernessUp(int $value): void {
             $this->cleverness += $value;
         }
 
-        //運をアップする
-        public function luckUp ($value) {
+        /**
+         * 勇者の運をアップする
+         *
+         * @param integer $value 運の増分
+         * @return void
+         */
+        public function luckUp (int $value): void {
             $this->luck += $value;
         }
 
-        //レベルを返す
-        public function getLevel() {
+        /**
+         * 勇者のレベルを取得する
+         *
+         * @return integer
+         */
+        public function getLevel(): int {
             return $this->level;
         }
-        //体力を返す
-        public function getphysicalStrength() {
+        /**
+         * 勇者の体力を取得する
+         *
+         * @return integer
+         */
+        public function getphysicalStrength(): int {
             return $this->physicalStrength;
         }
-        //攻撃力を返す
-        public function getOffensivePower() {
+        /**
+         * 勇者の攻撃力を取得する
+         *
+         * @return integer
+         */
+        public function getOffensivePower(): int {
             return $this->offensivePower;
         }
-        //防御力を返す
-        public function getDefense() {
+        /**
+         * 勇者の防御力を取得する
+         *
+         * @return integer
+         */
+        public function getDefense(): int {
             return $this->defense;
         }
-        //素早さを返す
-        public function getAgility() {
+        /**
+         * 勇者の素早さを取得する
+         *
+         * @return integer
+         */
+        public function getAgility(): int {
             return $this->agility;
         }
-        //賢さを返す
-        public function getCleverness() {
+        /**
+         * 勇者の賢さを取得する
+         *
+         * @return integer
+         */
+        public function getCleverness(): int {
             return $this->cleverness;
         }
-        //運を返す
-        public function getLuck() {
+        /**
+         * 勇者の運を取得する
+         *
+         * @return integer
+         */
+        public function getLuck(): int {
             return $this->luck;
         }
 
-        //levelup h a d s c f
+        /**
+         * levelupイベント
+         * 
+         * 勇者の全てのステータスをアップさせる（レベルの増分は1で固定）
+         * @param integer $physicalStrength 体力の増分
+         * @param integer $offensivePower   攻撃力の増分
+         * @param integer $defense          防御力の増分
+         * @param integer $agility          素早さの増分
+         * @param integer $cleverness       賢さの増分
+         * @param integer $luck             運の増分
+         * @return void
+         */
         public function levelUpEvent(
             int $physicalStrength,
             int $offensivePower,
@@ -87,7 +173,7 @@
             int $agility,
             int $cleverness,
             int $luck
-            )
+            ): void
         {
             $this->levelUp();
             $this->physicalStrengthUP($physicalStrength);
@@ -98,25 +184,51 @@
             $this->luckUp($luck);
         }
 
-        //muscle_training h a
-        public function muscleTrainingEvent(int $physicalStrength, int $offensivePower) {
+        /**
+         * muscle_trainingイベント
+         * 
+         * 勇者の体力、攻撃力をアップさせる
+         * @param integer $physicalStrength 体力の増分
+         * @param integer $offensivePower   攻撃力の増分
+         * @return void
+         */
+        public function muscleTrainingEvent(int $physicalStrength, int $offensivePower): void {
             $this->physicalStrengthUP($physicalStrength);
             $this->offensivePowerUp($offensivePower);
         }
 
-        //running d s
-        public function runningEvent(int $defense, int $agility) {
+        /**
+         * runningイベント
+         *
+         * 勇者の防御力、素早さをアップさせる
+         * @param integer $defense 防御力の増分
+         * @param integer $agility 素早さの増分
+         * @return void
+         */
+        public function runningEvent(int $defense, int $agility): void {
             $this->defenseUp($defense);
             $this->agilityUp($agility);
         }
 
-        //study c
-        public function studyEvent(int $cleverness) {
+        /**
+         * studyイベント
+         *
+         * 勇者の賢さをアップさせる
+         * @param integer $cleverness 賢さの増分
+         * @return void
+         */
+        public function studyEvent(int $cleverness): void {
             $this->clevernessUp($cleverness);
         }
 
-        //pray f
-        public function prayEvent(int $luck) {
+        /**
+         * prayイベント
+         *
+         * 勇者の運をアップさせる
+         * @param integer $luck 運の増分
+         * @return void
+         */
+        public function prayEvent(int $luck): void {
             $this->luckUp($luck);
         }
     }
@@ -156,9 +268,13 @@
     //イベントの処理を行う
     for ($i = 1; $i <= $eventCount; $i ++) {
 
+        //入力を取得
         $input = trim(fgets(STDIN));
+        //入力をスペース区切りで配列に変換
         $arrayInput = explode(" ", $input);
+        //勇者番号を取得
         $braveId = $arrayInput[0];
+        //イベントの種類を取得
         $event = $arrayInput[1];
 
         //該当の勇者インスタンス
@@ -191,6 +307,7 @@
                 $cleverness = $arrayInput[2];
                 $brave->studyEvent($cleverness);
                 break;
+
             case 'pray':
                 $luck = $arrayInput[2];
                 $brave->prayEvent($luck);
