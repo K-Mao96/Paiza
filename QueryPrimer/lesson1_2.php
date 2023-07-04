7946cdd08729eb07195bf8aec49971370e0362e9
K.M <indiangirl.0914@gmail.com>
2023-07-04 22:10:06 +0900

    リファクタリング

--- QueryPrimer/lesson1_2.php
+++ QueryPrimer/lesson1_2.php
@@ -7,29 +7,32 @@
  * @link https://paiza.jp/works/mondai/query_primer/query_primer__single_search/edit?language_uid=php
  */
 
-    // 数字の総数,
-    // ターゲットとなる数字 を取得
-    fscanf(STDIN, "%d %d", $itemCount, $target);
+    // 数字の総数とターゲットとなる数字を取得
+    // MEMO: 分割代入を活用
+    [$itemCount, $target] = fscanf(STDIN, "%d %d");
     
-    // デフォルトの答えを「NO」にする
-    $result = 'NO';
+    // ターゲットの数字が見つかったかどうかを示すフラグ変数
+    // MEMO: 「リーダブルコード」, p.34
+    // bool値の変数名にはis/has/can/shouldなどをつけてわかりやすくする
+    $hasTarget = false;
     
-    // ターゲットの数字が見つかった時点でYesを表示する
     for ($i=0; $i<$itemCount; $i++) {
         
         // 対象の数字を取り出す
         fscanf(STDIN, "%d", $item);
         
-        // ターゲットと一致したら答えをYesにしてループを抜ける
+        // 数字を取得し、ターゲットが存在する場合はフラグ変数をtrueにしてループを抜ける
+        // MEMO: 「良いコード悪いコード..」, p.133
+        // 早期breakでネストを解消する
         if ($item == $target) {
-            $result = 'YES';
+            $hasTarget = true;
             break;
         }
         
     }
     
     // 答えを表示する
-    echo $result;
+    echo $hasTarget ? 'YES' : 'NO';
     
     
 ?>
