3531e9f39088918c8a9d014d4ed3da11306ebb7e
K.M <indiangirl.0914@gmail.com>
2023-07-06 23:13:18 +0900

    計算量が少なくなるようリファクタリング

--- QueryPrimer/lesson1_3.php
+++ QueryPrimer/lesson1_3.php
@@ -4,30 +4,24 @@
  * piza「クエリメニュー」
  * ソートと検索 (query)
  * STEP: 2 指定要素の検索 (query)
- * @link https://paiza.jp/works/mondai/query_primer/query_primer__single_search/edit?language_uid=php
+ * @link https://paiza.jp/works/mondai/query_primer/query_primer__multi_search/edit?language_uid=php&t=ae4ac4ad1f5d0acb11ae22b904512ab0
  */
 
     // 配列A、Bの要素数を取得する
-    [$arrayASize, $arrayBSize] = fscanf(STDIN, "%d %d");
+    [$arrayASize, $targetSize] = fscanf(STDIN, "%d %d");
     
     // 配列Aを取得する
     $arrayA = [];
     for ($i=1; $i<=$arrayASize; $i++) {
         fscanf(STDIN, "%d", $item);
-        $arrayA[] = $item;
+        $arrayA[$item] = true;
     }
     
-    // 配列Bを取得する
-    $arrayB = [];
-    for ($i=1; $i<=$arrayBSize; $i++) {
-        fscanf(STDIN, "%d", $item);
-        $arrayB[] = $item;
+    // 配列Aの中にターゲットがあったらYES、なければNOを出力する
+    // MEMO: 計算量がO(Q log N)になるようにリファクタリング
+    for ($i=1; $i<=$targetSize; $i++) {
+        fscanf(STDIN, "%d", $target);
+        echo isset($arrayA[$target]) ? "YES\n" : "NO\n";
     }
-    
-    // 配列Aの中に配列Bの要素があったらYES、なければNOを出力する
-    foreach($arrayB as $itemB) {
-        echo in_array($itemB, $arrayA) ? "YES\n" : "NO\n";
-    }
-    
-    
+
 ?>
