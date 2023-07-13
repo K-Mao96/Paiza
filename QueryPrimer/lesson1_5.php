971395ff015d55e63bbdda4a38d8fc59e0720476
K.M <indiangirl.0914@gmail.com>
2023-07-13 21:01:14 +0900

    初回解答

---
+++ QueryPrimer/lesson1_5.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * piza「クエリメニュー」
+ * ソートと検索 (query)
+ * STEP: 5 先頭の要素の削除
+ * @link https://paiza.jp/works/mondai/query_primer/query_primer__multi_pop/edit?language_uid=php
+ */
+
+// 数字の総数を取得
+// 命令の総数を取得
+$input = trim(fgets(STDIN));
+$inputs = explode(' ', $input);
+
+$numberListSize = $inputs[0];
+$commandListSize = $inputs[1];
+
+// 数字のリストを作る
+$numberList = [];
+for ($i=0; $i<$numberListSize; $i++) {
+    fscanf(STDIN, "%d", $number);
+    $numberList[] = $number;
+}
+
+// 命令のリストを取得する
+$commandList = [];
+for ($i=0; $i<$commandListSize; $i++) {
+    fscanf(STDIN, "%s", $command);
+    $commandList[] = $command;
+}
+
+// 配列の要素を出力する関数
+function echoNumber(int $number) {
+    echo $number . "\n";
+}
+
+// 命令に沿って配列を操作する
+foreach ($commandList as $command) {
+    if ($command === "pop") {
+        array_shift($numberList);
+    }
+    if ($command === "show") {
+        array_map('echoNumber', $numberList);
+    }
+}
+
+
+?>
