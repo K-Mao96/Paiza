18495e3a9e96a43baac5d2d044165480cb1966b7
K.M <indiangirl.0914@gmail.com>
2023-08-06 12:00:31 +0900

    「身長」の値オブジェクトを作成

--- QueryPrimer/lesson1_8.php
+++ QueryPrimer/lesson1_8.php
@@ -7,15 +7,19 @@
  * @link https://paiza.jp/works/mondai/query_primer/query_primer__sort_find_single/edit?language_uid=php
  */
 
+// 例外クラス
+class StudentCountException extends Exception {}
+class HeightException extends Exception {}
+
 // 生徒数クラス
 class StudentCount {
     public function __construct(private int $value)
     {
         if ($value < 1) {
-            throw new Exception("生徒数には1以上を指定してください");
+            throw new StudentCountException("生徒数には1以上を指定してください");
         }
         if ($value > 100000) {
-            throw new Exception("生徒数には100,000以下を指定してください");
+            throw new StudentCountException("生徒数には100,000以下を指定してください");
         }
         $this->value = $value;
     }
@@ -26,31 +30,66 @@ class StudentCount {
     }
 }
 
+// 身長クラス
+class Height {
+    public function __construct(private int $value)
+    {
+        if ($value < 100) {
+            throw new HeightException("身長には100cm以上を指定してください");
+        }
+        if ($value > 200) {
+            throw new HeightException("身長には200cm以下を指定してください");
+        }
+        $this->value = $value;
+    }
 
-// N、X、Pを取得する
-fscanf(STDIN, "%d %d %d", $N, $newStudentHeight, $PaizaHeight);
-
-$studentCountObj = new StudentCount($N);
-$studentCount = $studentCountObj->getValue();
-
-// 生徒の身長を配列で取得
-$studentHeightList = [];
-for ($i=0; $i<$studentCount; $i++) {
-    fscanf(STDIN, "%d", $studentHeight);
-    $studentHeightList[] = $studentHeight;
+    public function getValue(): int
+    {
+        return $this->value;
+    }
 }
 
+// N、X、Pを取得する
+try {
+    fscanf(STDIN, "%d %d %d", $N, $X, $P);
 
-// 配列にPaizaくんの身長を追加
-$studentHeightList[] = $PaizaHeight;
-// 配列に転校生の身長を追加
-$studentHeightList[] = $newStudentHeight;
+    $studentCountObj = new StudentCount($N);
+    $studentCount = $studentCountObj->getValue();
 
-// 配列を身長の順に並べ替える
-sort($studentHeightList);
+    $newStudentHeightObj = new Height($X);
+    $newStudentHeight = $newStudentHeightObj->getValue();
 
-// Paizaくんの身長が前から何番目か調べる
-// MEMO: 0番目, 1番目... というカウント方法になっているため、1を足す
-echo array_search($PaizaHeight, $studentHeightList) + 1;
+    $paizaHeightObj = new Height($P);
+    $paizaHeight = $paizaHeightObj->getValue();
+
+    // 生徒の身長を配列で取得
+    $studentHeightList = [];
+    for ($i=0; $i<$studentCount; $i++) {
+        fscanf(STDIN, "%d", $studentHeight);
+        $studentHeightList[] = $studentHeight;
+    }
+
+
+    // 配列にPaizaくんの身長を追加
+    $studentHeightList[] = $paizaHeight;
+    // 配列に転校生の身長を追加
+    $studentHeightList[] = $newStudentHeight;
+
+    // 配列を身長の順に並べ替える
+    sort($studentHeightList);
+
+    // Paizaくんの身長が前から何番目か調べる
+    // MEMO: 0番目, 1番目... というカウント方法になっているため、1を足す
+    echo array_search($paizaHeight, $studentHeightList) + 1;
+
+} catch (StudentCountException $e) {
+    echo 'Error:' . $e->getMessage() . "\n";
+
+} catch (HeightException $e) {
+    echo 'Error:' . $e->getMessage() . "\n";
+
+} catch (Exception $e) {
+    echo 'Error:' . $e->getMessage() . "\n";
+}
 
 ?>
