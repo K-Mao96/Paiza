cdf21e0c5ffbd04361257fb12ad64a513dd933be
K.M <indiangirl.0914@gmail.com>
2023-08-06 11:44:46 +0900

    「生徒数」の値オブジェクトを作成

--- QueryPrimer/lesson1_8.php
+++ QueryPrimer/lesson1_8.php
@@ -7,8 +7,31 @@
  * @link https://paiza.jp/works/mondai/query_primer/query_primer__sort_find_single/edit?language_uid=php
  */
 
+// 生徒数クラス
+class StudentCount {
+    public function __construct(private int $value)
+    {
+        if ($value < 1) {
+            throw new Exception("生徒数には1以上を指定してください");
+        }
+        if ($value > 100000) {
+            throw new Exception("生徒数には100,000以下を指定してください");
+        }
+        $this->value = $value;
+    }
+
+    public function getValue(): int
+    {
+        return $this->value;
+    }
+}
+
+
 // N、X、Pを取得する
-fscanf(STDIN, "%d %d %d", $studentCount, $newStudentHeight, $PaizaHeight);
+fscanf(STDIN, "%d %d %d", $N, $newStudentHeight, $PaizaHeight);
+
+$studentCountObj = new StudentCount($N);
+$studentCount = $studentCountObj->getValue();
 
 // 生徒の身長を配列で取得
 $studentHeightList = [];
