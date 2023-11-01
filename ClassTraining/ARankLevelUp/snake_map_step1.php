a7b356d93ff3ed28ea25ef3a9d4390c1ab5b012e
K.M <indiangirl.0914@gmail.com>
2023-11-01 22:24:10 +0900

    処理をクラス化

--- ClassTraining/ARankLevelUp/snake_map_step1.php
+++ ClassTraining/ARankLevelUp/snake_map_step1.php
@@ -6,32 +6,239 @@
  * @link https://paiza.jp/works/mondai/a_rank_level_up_problems/a_rank_snake_map_step1/edit?language_uid=php
  */
 
+class RowAmoutException extends Exception {}
+class ColAmoutException extends Exception {}
+class TextAmoutException extends Exception {}
+
+// 盤面の行数クラス
+    // 1<=H<=20
+class Row {
+    private int $value;
+
+    public function __construct(int $value)
+    {
+        if ($value < 1) {
+            throw new RowAmoutException("行数には1以上を指定してください");
+        }
+        if ($value > 20) {
+            throw new RowAmoutException("行数には20以下を指定してください");
+        }
+        $this->value = $value;
+    }
+
+    public function getValue()
+    {
+        return $this->value;
+    }
+}
+
+// 盤面の列数クラス
+    //1<=W<=20
+class Col {
+    private int $value;
+
+    public function __construct(int $value)
+    {
+        if ($value < 1) {
+            throw new ColAmoutException("列数には1以上を指定してください");
+        }
+        if ($value > 20) {
+            throw new ColAmoutException("列数には20以下を指定してください");
+        }
+        $this->value = $value;
+    }
+
+    public function getValue()
+    {
+        return $this->value;
+    }
+}
+
+// 盤面の文字列クラス
+    // .か#
+class Text {
+    private string $value;
+
+    public function __construct(string $value)
+    {
+        if ($value !== "." && $value !== "#") {
+            throw new TextAmoutException("盤面の文字には.か#のみを指定してください。");
+        }
+
+        $this->value = $value;
+    }
+
+    public function getValue()
+    {
+        return $this->value;
+    }
+}
+
+// 盤面クラス
+    // 配列
+class Board {
+    private array $board;
+
+    public function __construct(Row $row)
+    {
+        $row = $row->getValue();
+
+        for ($i=0; $i<$row; $i++) {
+            // 各行の文字列を取得する
+            fscanf(STDIN, "%s", $text);
+        
+            // 1文字ずつ区切って配列にする
+            $textList = str_split($text);
+        
+            $this->board[$i] = $textList;
+        }
+    }
+
+    public function getBoard()
+    {
+        return $this->board;
+    }
+
+    public function getBoardText(XCoordinate $x, YCoordinate $y)
+    {
+        $xValue = $x->getValue();
+        $yValue = $y->getValue();
+
+        $result = $this->board[$yValue][$xValue];
+
+        return $result;
+    }
+}
+
+// 座標の数クラス
+    // 1<=N<=H*W
+class CoordinateAmount {
+    private string $value;
+
+    public function __construct(int $value, Row $row, Col $col)
+    {
+        $height = $row->getValue();
+        $width = $col->getValue();
+        $maxAmount = $height * $width;
+        if ($value < 1) {
+            throw new TextAmoutException("座標の数には1以上を指定してください。");
+        }
+
+        if ($value > $maxAmount) {
+            throw new TextAmoutException("座標の数には" . $maxAmount . "以下を指定してください。");
+        }
+
+
+        $this->value = $value;
+    }
+
+    public function getValue()
+    {
+        return $this->value;
+    }
+}
+
+// Y座標クラス
+    // 0 ≦ y < H
+class YCoordinate {
+    private string $value;
+
+    public function __construct(int $value, Row $row)
+    {
+        $height = $row->getValue();
+        if ($value < 0) {
+            throw new TextAmoutException("y座標には0以上を指定してください。");
+        }
+
+        if ($value > $height) {
+            throw new TextAmoutException("y座標には" . $height . "より小さい値を指定してください。");
+        }
+
+        $this->value = $value;
+    }
+
+    public function getValue()
+    {
+        return $this->value;
+    }
+}
+
+// X座標クラス
+    // 0 ≦ x < W 
+class XCoordinate {
+    private string $value;
+
+    public function __construct(int $value, Col $col)
+    {
+        $width = $col->getValue();
+        if ($value < 0) {
+            throw new TextAmoutException("x座標には0以上を指定してください。");
+        }
+
+        if ($value > $width) {
+            throw new TextAmoutException("x座標には" . $width . "より小さい値を指定してください。");
+        }
+
+        $this->value = $value;
+    }
+
+    public function getValue()
+    {
+        return $this->value;
+    }
+}
+
+// 処理実行クラス
+class DrawBoardText
+{
+    private CoordinateAmount $coordinateAmountObj;
+    private Row $rowObj;
+    private Col $colObj;
+    private Board $boardObj;
+
+    public function __construct(CoordinateAmount $coordinateAmountObj, Row $rowObj, Col $colObj, Board $boardObj)
+    {
+        $this->coordinateAmountObj = $coordinateAmountObj;
+        $this->rowObj = $rowObj;
+        $this->colObj = $colObj;
+        $this->boardObj = $boardObj;
+    }
+
+    public function exec()
+    {
+        $coordinateAmount = $this->coordinateAmountObj->getValue();
+        for ($i=0; $i<$coordinateAmount; $i++) {
+            // 各行の入力を取得する（y座標、x座業）
+            // 半角スペースで区切って別々の変数として定義する
+            [$y, $x] = fscanf(STDIN, "%d %d");
+            $yObj = new YCoordinate($y, $this->rowObj);
+            $xObj = new XCoordinate($x, $this->colObj);
+            // 配列Aに対して、座標と同じインデックスの要素を文字列をして出力する
+            $result = $this->boardObj->getBoardText($xObj, $yObj);
+            $text = new Text($result);
+            echo $text->getValue();
+            // 改行を出力する
+            echo "\n";
+        }
+    }
+
+}
+
 
 // 盤面の行数H、列数W、座標の数Nを取得する
-[$row, $col, $coordinateAmount] = fscanf(STDIN, "%d %d %d");
+[$h, $w, $n] = fscanf(STDIN, "%d %d %d");
+
+$rowObj = new Row($h);
+$colObj = new Col($w);
+$coordinateAmountObj = new CoordinateAmount($n, $rowObj, $colObj);
 
 // 盤面を表現する配列Aを空で定義
-$board = [];
-// H回繰り返す
-for ($i=0; $i<$row; $i++) {
-    // 各行の文字列を取得する
-    fscanf(STDIN, "%s", $text);
+$boardObj = new Board($rowObj);
 
-    // 1文字ずつ区切って配列Bとして定義
-    $textList = str_split($text);
+// 座標の数
+$coordinateAmount = $coordinateAmountObj->getValue();
 
-    // 配列Aに配列Bを要素として追加
-    $board[$i] = $textList;
-}
-
-// N回繰り返す
-for ($i=0; $i<$coordinateAmount; $i++) {
-    // 各行の入力を取得する（y座標、x座業）
-    // 半角スペースで区切って別々の変数として定義する
-    [$y, $x] = fscanf(STDIN, "%d %d");
-    // 配列Aに対して、座標と同じインデックスの要素を文字列をして出力する
-    echo $board[$y][$x];
-    // 改行を出力する
-    echo "\n";
-}
+// ボードの文字を出力する
+$drawBoardTextObj = new DrawBoardText($coordinateAmountObj, $rowObj, $colObj, $boardObj);
+$drawBoardTextObj->exec();
 ?>
